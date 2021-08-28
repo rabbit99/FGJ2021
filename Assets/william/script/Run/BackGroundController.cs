94bf84117521a8a7332f82a3e880574099ff9af2
William9527 <vvbest2012@gmail.com>
2021-08-28 12:40:10 +0800

    Run
    loop背景

--- Assets/william/script/Run/BackGroundController.cs
+++ Assets/william/script/Run/BackGroundController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BackGroundController : MonoBehaviour
 {
     public GameObject[] backGrounds;
+    public float back_ground_move_speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,11 @@ public class BackGroundController : MonoBehaviour
     {
         foreach(var go in backGrounds)
         {
-            go.transform.Translate(new Vector3(-1, 0, 0));
+            go.transform.Translate(new Vector3(back_ground_move_speed, 0, 0));
+            if(go.transform.position.x < -27)
+            {
+                go.transform.SetPositionAndRotation(new Vector3(53.8f,0,0),Quaternion.identity);
+            }
         }
     }
 }

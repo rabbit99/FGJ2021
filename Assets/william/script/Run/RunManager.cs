c807fe209ec096033bc9a963c16f1af1109f5c99
William9527 <vvbest2012@gmail.com>
2021-08-28 15:53:05 +0800

    Run
    障礙物生成，Game Over邏輯

--- Assets/william/script/Run/RunManager.cs
+++ Assets/william/script/Run/RunManager.cs
@@ -11,7 +11,10 @@ public class RunManager : MonoBehaviour
     private CooldownTimer cooldownTimer;
 
     public UnityEvent<float> SanUpateAction;
+    public UnityEvent HpUpateAction;
     public UnityEvent OverAction;
+
+    private int hp = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +37,27 @@ public class RunManager : MonoBehaviour
         sanData.san += san_amount;
         SanUpateAction?.Invoke(sanData.san);
         if (sanData.IsOver())
+        {
+            cooldownTimer.Pause();
+            OverAction?.Invoke();
+        }
+    }
+
+    public void IncreaseSan(float value)
+    {
+        sanData.san += value;
+        SanUpateAction?.Invoke(sanData.san);
+        if (sanData.IsOver())
+        {
+            cooldownTimer.Pause();
+            OverAction?.Invoke();
+        }
+    }
+
+    public void Hurt(int value)
+    {
+        hp -= value;
+        if (hp <= 0)
         {
             cooldownTimer.Pause();
             OverAction?.Invoke();
